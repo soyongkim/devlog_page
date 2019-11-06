27ccf75c891cee294ec21832ea790204d88a3c08
soyongkim <thdyd324@gmail.com>
2019-11-06 19:41:05 +0900

    first commit

---
+++ search.min.72ff90f29aea8caffbac5189e4a3ac0b45a6b308848ccee801041217b6b9f52f.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/search-data.min.3f829d128b4b4bf67d2157a6ba0b368aba2d2d731683d4be98bb5f0f70c19e27.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
